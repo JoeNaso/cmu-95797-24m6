fdc0d2cf2f525a0691aad94c3357a84c6570a578
Joe Naso <jnaso3@gmail.com>
2025-04-18 18:13:51 -0400

    start script, WIP docs

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # cmu-95797-23m6
+
+## Setup
+To get started, you will need a local environment to run python. You can set this up by following these steps: 
+1. Download this project to you computer. You will need git in order to do so. 
+   1. Git setup: `TODO`
+2. Download `uv`. This is an open-source package manager which will let you set up all of the project dependencies without effecting the rest of your computer's setup. 
+   1. Windows: [Installation instructions](https://docs.astral.sh/uv/getting-started/installation/#__tabbed_1_2)
+   2. Mac: [Installation instructions](https://docs.astral.sh/uv/getting-started/installation/#__tabbed_1_1)
+      1. You can also use Homebrew on Mac to easily install, but this might require other updates
+3. Run the setup script (You only have to do this once)
+   1. In your terminal, navigate to the root of this project (the same folder this file resides) and run the following command: 
+      1. `uv run ./scripts/start_project.py`
+      2. Note: Running this script multiple times will be fine, but you only have to do this once! 
+   2. This will create a virtual environment which can be access to run the project
+      1. To use the virtual environment navigate to the project root. then:
+         1. Windows: `.venv\Scripts\activate`
+         2. Mac: `source ./venv/bin/activate`
+   
+
+
+`TODO`: CONTINUE
+
+VSCODE Plugins
--- scripts/start_project.py
+++ scripts/start_project.py
@@ -6,20 +6,30 @@ import os
 from pathlib import Path
 import time
 import subprocess
+import shutil
 
 
 ENV = ".env"
 VENV = ".venv"
+PY_VERSION = "python3.11"
 THIS_PROJECT = Path(__file__).resolve().parent.parent
+
+
+def _msg(steps: list[str], max_len: int) -> None:
+    for step in steps:
+        print(f"| {step.ljust(max_len)} |")
+        time.sleep(1)
 
 
 def _env():
     """
     Write a .env file
-    """    
+    """
     if not os.path.exists(os.path.join(os.sep, THIS_PROJECT, ".env")):
-        with open(THIS_PROJECT / ".env", 'w') as f:
-            f.write("# Caution: if you change this value, your warehouse project may not work as expected")
+        with open(THIS_PROJECT / ".env", "w") as f:
+            f.write(
+                "# Caution: if you change this value, your warehouse project may not work as expected"
+            )
             f.write(f"CMU_WH_LOCAL_SOURCE_DATA={THIS_PROJECT}/source_data/")
 
 
@@ -28,40 +38,62 @@ def _setup_env():
     create a virtual env for later usage
     since we cant really write the required env vars into the activate script cleanly, users must source the env manually
     """
+    local_uv = subprocess.run(
+        ["which", "uv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+    )
+    if local_uv.stderr != "":
+        raise ValueError(
+            "Please make sure you install `uv` ony our computer before running this setup command\n",
+            "Install instructions: \n\t[Windows] https://docs.astral.sh/uv/getting-started/installation/#__tabbed_1_2",
+            "\n\t[Mac] https://docs.astral.sh/uv/getting-started/installation/#__tabbed_1_1",
+        )
+    uv_path = local_uv.stdout.strip()
+
     venv_dir = os.path.join(os.sep, THIS_PROJECT, VENV)
     if os.path.exists(venv_dir):
-        os.removedirs(venv_dir)
+        shutil.rmtree(venv_dir)
     if not os.getcwd() == THIS_PROJECT:
         os.chdir(THIS_PROJECT)
-    subprocess.run(["uv", "venv", str(VENV)], check=True)
+    subprocess.run([f"{uv_path}", "venv", VENV, f"--python={PY_VERSION}"], check=True, stdout=subprocess.PIPE)
+    installs = [
+        f"{uv_path}",
+        "pip",
+        "install",
+        "-r",
+        "pyproject.toml",
+    ]
 
-    subprocess.run(["uv", "pip", "install", "-r", "pyproject.toml"], check=True)
-
-
-
-
-def welcome_message():
-    banner = "Beginning CMU-85797 Warehouse setup..."
-    one = "Please make sure you have downloaded the source data ZIP file and placed in the `source_data` folder of this project"
-    two = "This script will set up an environment on your computer so you can run the project"
-    three = "Refer to the `README.md` after this process completes."
-    directions = [one, two, three]
-    max_len = max(len(line) for line in directions)
-    border = "=" * (len(one) + 4)
-    print("\n" + border)
-    for step in [banner, "", *directions]:
-        print(f"| {step.ljust(max_len)} |")
-        time.sleep(1)
-    print(border + "\n")    
+    subprocess.run(
+        installs,
+        env={"VIRTUAL_ENV": venv_dir},
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
 
 
 def start():
+    banner = "Beginning CMU-85797 Warehouse setup..."
+    one = "Please make sure you have downloaded the source data ZIP file and placed in the `source_data` folder of this project"
+    uv = "You also need to install `uv` in order to continue. Refer to the README.md for more details"
+    two = "This script will set up an environment on your computer so you can run the project"
+    three = "Refer to the `README.md` after this process completes."
+    directions = [one, uv, two, three]
+    max_len = max(len(line) for line in directions)
+    border = "=" * (max_len + 4)
+    buffer = ["",""]
+    print("\n" + border)
+    _msg([banner, "", *directions], max_len)
     _env()
+    _msg(["Setting up environment... this might take a minute", *buffer], max_len)
     _setup_env()
-
-    # welcome_message()
-
-
+    _msg([*buffer, "Installations complete!"], max_len)
+    complete = [
+        "Virtual environemnt setup complete.",
+        "You can start development on your Warehouse project. Please refer to the README.md to get started",
+    ]
+    _msg(complete, max_len)
+    print(border + "\n")
 
 
 if __name__ == "__main__":
